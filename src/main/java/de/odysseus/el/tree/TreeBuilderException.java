08bf04ba184a52a672ab92cf7af5ae00d6f4bfda
Christoph Beck <beck@odysseus.de>
2009-07-19 15:30:32 +0000

    updated copyright notice

--- src/main/java/de/odysseus/el/tree/TreeBuilderException.java
+++ src/main/java/de/odysseus/el/tree/TreeBuilderException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Odysseus Software GmbH
+ * Copyright 2006-2009 Odysseus Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
